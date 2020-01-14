39f87457ff86cb63f5fb031b53a7bda0bf37d0ed
masoud <masoud.kn@yahoo.com>
2020-01-14 19:37:39 +0330

    Main M-file . Head File

--- Main.m
+++ Main.m
@@ -1,15 +1,14 @@
 clc; clear ;close all;
 
-
-t=datestr(now);
 disp(['>>========================================<<'])
-disp(['>>==========',t,'==========<<'])
+disp(['>>==========',datestr(now),'==========<<'])
 disp(['>>========================================<<'])
 tic
 
 xml_eph= xml2struct('gps_ephemeris.xml');
-% eph    = eph_data(xml_eph);
-% sat_pv = sat_pv_cal(---)
+eph    = eph_data(xml_eph);
+sat_pv = sat_pv_cal(eph);
+
 
 
 
