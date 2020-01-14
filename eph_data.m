ffce86f72452ef96abe20bea4164a4587fc67fbf
masoud <masoud.kn@yahoo.com>
2020-01-14 19:30:29 +0330

    Matlab M-File to organize ephemeris data. "Final"

--- eph_data.m
+++ eph_data.m
@@ -1,26 +1,38 @@
 %% preParing Of Ephemerids Data============================================
 function eph=eph_data(input)
-    count = str2double(input.boostu_serialization{1,2}.GNSSu_dashu_SDRu_ephemerisu_map.count.Text);
-%     temp = input.boostu_serialization{1,2}.GNSSu_dashu_SDRu_ephemerisu_map.item;  
-%     for i=1:count
-%         eph.prn(i,1)    =str2double(); 
-%         eph.c_rs(i,1)   =str2double();
-%         eph.delta_n(i,1)=str2double();
-%         eph.M_0(i,1)    =str2double();
-%         eph.c_us(i,1)   =str2double();
-%         eph.c_uc(i,1)   =str2double();
-%         eph.eps(i,1)    =str2double();
-%         eph.A_sqrt(i,1) =str2double();
-%         eph.t_oe(i,1)   =str2double();
-%         eph.c_ic(i,1)   =str2double();
-%         eph.w_0(i,1)    =str2double();
-%         eph.c_is(i,1)   =str2double();
-%         eph.i_0(i,1)    =str2double();
-%         eph.c_rc(i,1)   =str2double();
-%         eph.w(i,1)      =str2double();
-%         eph.w_dot(i,1)  =str2double();
-%         eph.IDOT(i,1)   =str2double();       
-%     end
+    gpspi  =3.14159265359;
+    count = str2double(input.boostu_serialization{1,2}. ...
+                           GNSSu_dashu_SDRu_ephemerisu_map.count.Text);
+    temp = input.boostu_serialization{1,2}. ...
+                           GNSSu_dashu_SDRu_ephemerisu_map.item;  
+                       
+    for i=1:count
+        eph.count(1,i)  =count;
+        eph.prn(1,i)    =str2double(temp{1,i}.second.iu_satelliteu_PRN.Text); 
+        eph.c_rs(1,i)   =str2double(temp{1,i}.second.du_Crs.Text);
+        eph.delta_n(1,i)=str2double(temp{1,i}.second.du_Deltau_n.Text)*gpspi/180;
+        eph.M_0(1,i)    =str2double(temp{1,i}.second.du_Mu_0.Text)*gpspi/180;
+        eph.c_us(1,i)   =str2double(temp{1,i}.second.du_Cus.Text);
+        eph.c_uc(1,i)   =str2double(temp{1,i}.second.du_Cuc.Text);
+        eph.eps(1,i)    =str2double(temp{1,i}.second.du_eu_eccentricity.Text);
+        eph.A_sqrt(1,i) =str2double(temp{1,i}.second.du_sqrtu_A.Text);
+        eph.t_oe(1,i)   =str2double(temp{1,i}.second.du_Toe.Text);
+        eph.c_ic(1,i)   =str2double(temp{1,i}.second.du_Cic.Text);
+        eph.w_0(1,i)    =str2double(temp{1,i}.second.du_OMEGA0.Text)*gpspi/180;
+        eph.c_is(1,i)   =str2double(temp{1,i}.second.du_Cis.Text);
+        eph.i_0(1,i)    =str2double(temp{1,i}.second.du_iu_0.Text)*gpspi/180;
+        eph.c_rc(1,i)   =str2double(temp{1,i}.second.du_Crc.Text);
+        eph.w(1,i)      =str2double(temp{1,i}.second.du_OMEGA.Text)*gpspi/180;
+        eph.w_dot(1,i)  =str2double(temp{1,i}.second.du_OMEGAu_DOT.Text)*gpspi/180;
+        eph.IDOT(1,i)   =str2double(temp{1,i}.second.du_IDOT.Text)*gpspi/180;
+        eph.TOW(1,i)    =str2double(temp{1,i}.second.du_TOW.Text);
+        eph.t_oc(1,i)   =str2double(temp{1,i}.second.du_Toc.Text);
+        eph.a_f2(1,i)   =str2double(temp{1,i}.second.du_Au_f2.Text);
+        eph.a_f1(1,i)   =str2double(temp{1,i}.second.du_Au_f1.Text);
+        eph.a_f0(1,i)   =str2double(temp{1,i}.second.du_Au_f0.Text);
+        eph.TGD(1,i)    =str2double(temp{1,i}.second.du_TGD.Text);
+        eph.e(1,i)      =str2double(temp{1,i}.second.du_eu_eccentricity.Text);
+    end
 end
 
 
