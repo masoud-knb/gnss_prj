fa76e92436cb34723c4809f815c23ea4caf06b33
masoud <masoud.kn@yahoo.com>
2020-02-03 18:08:13 +0330

    The Head File. Some New Functions Added.

--- Main.m
+++ Main.m
@@ -5,13 +5,13 @@ disp(['>>==========',datestr(now),'==========<<'])
 disp(['>>========================================<<'])
 tic
 
-xml_eph= xml2struct('gps_ephemeris.xml');
-eph    = eph_data(xml_eph);
-sat_pv = sat_pv_cal(eph);
 
 
-
-
+settings            = initset();
+xml_eph             = xml2struct('gps_ephemeris.xml');
+eph                 = eph_data(xml_eph);
+[sat_pv,satclkcorr] = sat_pv_cal(eph);
+[ta_1,cost_func]    = DPE(sat_pv,eph,settings);
 
 
 toc
